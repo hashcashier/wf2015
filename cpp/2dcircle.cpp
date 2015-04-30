b48cd9062415c76c6be85088d6b3bbd1e7ddbd51
Rami Khalil <thesavagekoder@gmail.com>
2015-04-30 12:58:05 +0200

    Syntax fixes. GUC Logo

---
+++ cpp/2dcircle.cpp
@@ -0,0 +1,4 @@
+// From 3 Points
+// From 1 Line 2 Points
+// From 2 Lines 1 Point
+// From 3 Lines
--- cpp/gaussian_elimination.cpp
+++ cpp/gaussian_elimination.cpp
@@ -1,24 +1,21 @@
 double* GaussianElimination(int N, double **mat) {
-  int i, j, k, l; double t;
- 
+  int i, j, k, L; double t;
   for (i = 0; i < N - 1; i++) {
-    l = i;
+    L = i;
     for (j = i + 1; j < N; j++)
-      if (fabs(mat[j][i]) > fabs(mat[l][i]))
-        l = j;
-    // partial pivot
+      if (fabs(mat[j][i]) > fabs(mat[L][i]))
+        L = j;
     for (k = i; k <= N; k++)
-	  swap(mat[i][k], mat[l][k]);
+	  swap(mat[i][k], mat[L][k]);
     for (j = i + 1; j < N; j++)
       for (k = N; k >= i; k--)
         mat[j][k] -= (mat[i][k] * mat[j][i]) / mat[i][i];
   }
- 
   double *res = new double[N];
   for (j = N - 1; j >= 0; j--) {
     for (t = 0.0, k = j + 1; k < N; k++)
 	  t += mat[j][k] * res[k];
-    res[j] = (mat[j][N] - t) / mat[j][j]; // the answer is here
+    res[j] = (mat[j][N] - t) / mat[j][j];
   }
   return res;
 }
