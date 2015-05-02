57cf54e6f9b979b3ec740932928d954a7d80946c
Rami Khalil <thesavagekoder@gmail.com>
2015-05-02 16:51:19 +0200

    Chinese Remainder Theorem

--- cpp/chinese_remainder.cpp
+++ cpp/chinese_remainder.cpp
@@ -1,10 +1,10 @@
-// Algorithm
-// Algorithm
-// Algorithm
-// Algorithm
-// Algorithm
-// Algorithm
-// Algorithm
-// Algorithm
-// Algorithm
-// Algorithm
+// returns minimum X satisfying X = Ai mod Ni for all i
+int chinese_remainder(int *n, int *a, int len){
+	int p, i, prod = 1, sum = 0;
+	for (i = 0; i < len; i++)
+		prod *= n[i];
+	for (i = 0; i < len; i++)
+		p = prod / n[i],
+		sum += a[i] * inverse(p, n[i]) * p;
+	return sum % prod;
+}
