8b9e6d3ca6d0b42c343e13e83dd4005456f2f11b
Rami Khalil <thesavagekoder@gmail.com>
2015-05-03 17:27:28 +0200

    Point in polygon

--- cpp/point_in_poly.cpp
+++ cpp/point_in_poly.cpp
@@ -1,20 +1,16 @@
-// Algorithm
-// Algorithm
-// Algorithm
-// Algorithm
-// Algorithm
-// Algorithm
-// Algorithm
-// Algorithm
-// Algorithm
-// Algorithm
-// Algorithm
-// Algorithm
-// Algorithm
-// Algorithm
-// Algorithm
-// Algorithm
-// Algorithm
-// Algorithm
-// Algorithm
-// Algorithm
+// outside 0, inside 1, on the border 2
+int pt_in_poly(const vector<point> &p, const point &a) {
+	int n = p.size(), inside = false;
+	double Ax = real(a), Ay = imag(a);
+	for (int i=0, j=n-1; i<n; j=i++) {
+		double 	Ix = p[i].real(), Iy = p[i].imag(),
+						Jx = p[j].real(), Jy = p[j].imag();
+		point Ia = a - p[i], Ja = a - p[j];
+		if (abs(cross(Ia, Ja)) < eps && dot(Ia, Ja) < eps)
+			return 2;
+		if ((Iy <= Ay && Ay < Jy) || (Jy <= Ay && Ay < Iy))
+				if((Ax-Ix)*(Jy-Iy) < (Jx-Ix)*(Ay-Iy))
+					inside = !inside;
+	}
+	return inside;
+}
