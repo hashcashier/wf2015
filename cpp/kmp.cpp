0fcdaff04f1e538fd22593385f66d84fa0e6468f
KhaledHelmy <khaled.a.helmy@gmail.com>
2015-05-04 23:33:45 +0200

    Added KMP

--- cpp/kmp.cpp
+++ cpp/kmp.cpp
@@ -1,15 +1,35 @@
-// Algorithm
-// Algorithm
-// Algorithm
-// Algorithm
-// Algorithm
-// Algorithm
-// Algorithm
-// Algorithm
-// Algorithm
-// Algorithm
-// Algorithm
-// Algorithm
-// Algorithm
-// Algorithm
-// Algorithm
+#define MAXN 100000
+
+string text, pattern;
+
+int b[MAXN];
+vector<int> found;
+
+void kmpPreprocess() {
+    int i = 0, j = -1;
+    b[0] = -1;
+    while(i < pattern.size()) {
+        while(j >= 0 && pattern[i] != pattern[j]) {
+            j = b[j];
+        }
+        i ++;
+        j ++;
+        b[i] = j;
+    }
+}
+
+void kmpSearch() {
+    found.clear();
+    int i = 0, j = 0;
+    while(i < text.size()) {
+        while(j >= 0 && text[i] != pattern[j]) {
+            j = b[j];
+        }
+        i ++;
+        j ++;
+        if(j == pattern.size()) {
+            found.push_back(i - j);
+            j = b[j];
+        }
+    }
+}
