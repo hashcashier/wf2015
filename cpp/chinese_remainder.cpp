3a071d683c12b3ff6756c1fa818dfab572a3e724
Rami Khalil <thesavagekoder@gmail.com>
2015-05-06 11:14:30 +0200

    Chinese remainder

--- cpp/chinese_remainder.cpp
+++ cpp/chinese_remainder.cpp
@@ -1,8 +1,7 @@
 // returns minimum X satisfying X = Ai mod Ni for all i
 int chinese_remainder(int *n, int *a, int len){
 	int p, i, prod = 1, sum = 0;
-	for (i = 0; i < len; i++)
-		prod *= n[i];
+	for (i = 0; i < len; i++) prod *= n[i];
 	for (i = 0; i < len; i++)
 		p = prod / n[i],
 		sum += a[i] * inverse(p, n[i]) * p;
