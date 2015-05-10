e30836de77902dc2cc3d5f702e6437e462a41ecd
Rami Khalil <thesavagekoder@gmail.com>
2015-05-10 23:40:29 +0200

    sg

--- cpp/grundy_numbers.cpp
+++ cpp/grundy_numbers.cpp
@@ -3,4 +3,4 @@ int g(int u) { // no standard or general way to do it
 	   smallest non-negative number not found
 	   amongst followers/neighbors of u */
 	return mex(F(u));
-}
+} // for sum of games, xor all SG-values (nim)
