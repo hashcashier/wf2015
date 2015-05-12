3df38b6259b036e97bdfde2e71b5f6099a11bfee
Rami Khalil <thesavagekoder@gmail.com>
2015-05-12 15:00:00 +0200

    Inclusion-Exclusion principle

---
+++ cpp/inclusion_exclusion.cpp
@@ -0,0 +1,7 @@
+/*Generalizing the results of these examples gives the principle of inclusion–exclusion. To find the cardinality of the union of n sets:
+1. Include the cardinalities of the sets.
+2. Exclude the cardinalities of the pairwise intersections.
+3. Include the cardinalities of the triple-wise intersections.
+4. Exclude the cardinalities of the quadruple-wise intersections.
+5. Include the cardinalities of the quintuple-wise intersections.
+6. Continue, until the cardinality of the n-tuple-wise intersection is included (if n is odd) or excluded (n even).*/
