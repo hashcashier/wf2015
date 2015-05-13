6934d5adf7b837475ca5dcf3d72a72da20f092eb
Rami Khalil <thesavagekoder@gmail.com>
2015-05-13 17:21:55 +0200

    General Chinese Mostafa

--- cpp/chinese_general.cpp
+++ cpp/chinese_general.cpp
@@ -1,15 +1,15 @@
-LL posMod(LL num, LL mod) {
-	return num < 0 ? ((num%mod)+mod)%mod : num%mod;
-}// Complexity: O(n log(MAX(m_0, ..., m_(n-1)) )
-bool succ_sub(vLL &a, vLL &m, LL &x) {
-	LL a_tmp = posMod(a[0], m[0]);
-	for (int i = 1; i < m.size(); ++i) {
-		Triple<LL> gst = egcd<LL>(m[i-1], m[i]);
-		if (abs(a_tmp - a[i]) % gst.d)
-			return false;
-		a_tmp = posMod(a_tmp + (a[i] - a_tmp) / gst.d * gst.x * m[i-1],
-				m[i-1] / gst.d * m[i]);
-	}
-	x = a_tmp;
-	return true;
+void merge(long long a2, long long n2, long long &a1, long long &n1) {
+	long long p1, p2, r = ext_gcd(n1, n2, p1, p2), n = n1 / r * n2;
+	a1 = (a1 + mulmod(mulmod((a2 - a1) / r, n1, n), p1, n)) % n, n1 = n;
+}
+long long chinese_remainder() {
+	for (int i = 0; i < m; ++i)
+		for (int j = 0; j < i; ++j)
+			if ((as[i] - as[j]) % __gcd(ns[i], ns[j]) != 0)
+				return -1;
+	long long n = 1, a = 0;
+	for (int i = 0; i < m; ++i)
+		merge(as[i], ns[i], a, n);
+	if (a == 0) a += n;
+	return n - a;
 }
