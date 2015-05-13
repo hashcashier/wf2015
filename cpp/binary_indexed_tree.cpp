29ce1e608c58c11d9275aceebb5d9b89a3656109
KhaledHelmy <khaled.a.helmy@gmail.com>
2015-05-13 12:49:25 +0200

    Added BIT

---
+++ cpp/binary_indexed_tree.cpp
@@ -0,0 +1,42 @@
+/*
+Tree is 1-based
+Tree can be extended to N-dimensions
+*/
+
+#define MAXN 1000
+
+int N;
+int bit[MAXN];
+int bit2[MAXN][MAXN];
+
+void update(int idx, int val) {
+	for(int i=idx; i<=N; i+=(i & -i)) {
+		bit[i] += val;
+	}
+}
+
+int query(int idx) {
+	int ret = 0;
+	for(int i=idx; i>0; i-=(i & -i)) {
+		ret += bit[i];
+	}
+	return ret;
+}
+
+void update2(int x, int y, int val) {
+	for(int i=x; i<=N; i+=(i & -i)) {
+		for(int j=y; j<=N; j+=(j & -j)) {
+			bit2[i][j] += val;
+		}
+	}
+}
+
+int query2(int x, int y) {
+	int ret = 0;
+	for(int i=x; i>0; i-=(i & -i)) {
+		for(int j=y; j>0; j-=(j & -j)) {
+			ret += bit[i][j];
+		}
+	}
+	return ret;
+}
