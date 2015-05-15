484a7a9d507d0a6243390107a9adada157cd5ba6
Rami Khalil <thesavagekoder@gmail.com>
2015-05-15 12:08:13 +0200

    MEC style

--- cpp/minimum_ec.cpp
+++ cpp/minimum_ec.cpp
@@ -1,5 +1,5 @@
 STATE circlePoint(point & cen, double & r, point& p) {
-	double lensqr = lengthSqr(vec(cen,p));
+	double lensqr = lengthSqr(vec(cen, p));
 	if (fabs(lensqr - r * r) < EPS)
 		return BOUNDRY;
 	if (lensqr < r * r)
@@ -16,26 +16,26 @@ int ps, rs;
 long double rad;
 
 void mec() {
-        if (rs == 3) {
-                circle3(r[0], r[1], r[2], cen, rad);
-                return;
-        }
-        if (rs == 2 && ps == 0) {
-                circle2(r[0], r[1], cen, rad);
-                return;
-        }
-        if (!ps) {
-                cen = r[0];
-                rad = 0;
-                return;
-        }
-        ps--;
-        mec();
-        if (circlePoint(cen, rad, p[ps]) == OUT) {
-                r[rs++] = p[ps];
-                mec();
-                rs--;
-        }
-        ps++;
+	if (rs == 3) {
+		circle3(r[0], r[1], r[2], cen, rad);
+		return;
+	}
+	if (rs == 2 && ps == 0) {
+		circle2(r[0], r[1], cen, rad);
+		return;
+	}
+	if (!ps) {
+		cen = r[0];
+		rad = 0;
+		return;
+	}
+	ps--;
+	mec();
+	if (circlePoint(cen, rad, p[ps]) == OUT) {
+		r[rs++] = p[ps];
+		mec();
+		rs--;
+	}
+	ps++;
 
 }
