a646c7f9a9443bd7a53157dcac7586c58cd01068
Rami Khalil <thesavagekoder@gmail.com>
2015-05-15 22:04:05 +0200

    intersection bugs

--- cpp/line_segs_intersection.cpp
+++ cpp/line_segs_intersection.cpp
@@ -1,10 +1,10 @@
 enum event_type {
 	START, INTERS, END
 };
-struct cmplxCmp {
-	bool operator()(const point &a, const point &b) {
-		if(a.real() != b.real()) return a.real() < b.real();
-		return a.imag() < b.imag();
+struct cmplxCmpG {
+	bool operator()(const point &a, const point &b) const {
+		if(a.real() != b.real()) return a.real() > b.real();
+		return a.imag() > b.imag();
 	}
 };
 struct event {
@@ -12,37 +12,40 @@ struct event {
 	event(){}
 	event(point loc, event_type type, int r) :p(loc),t(type),ref(r){}
 	bool operator<(const event &o) const {
-		if(p != o.p) return cmplxCmp()(p, o.p);
-		if(t != o.t) return t < o.t;
-		if(ref != o.ref) return ref < o.ref;
-		return sref < o.sref;
+		if(p != o.p) return cmplxCmpG()(p, o.p);
+		if(t != o.t) return t > o.t;
+		if(ref != o.ref) return ref > o.ref;
+		return sref > o.sref;
 	}
 };
 typedef pair<line, int> idSeg;
+
 struct sweepLineCmp {
 	static double *x;
 	static int A, B;
-//	sweepLineCmp(double *pos):x(pos){}
+	static set<ii> lessThan;
 	bool operator()(const idSeg &a, const idSeg &b) {
-		if(a.second == A && b.second == B) return true;
-		if(a.second == B && b.second == A) return false;
-		if(a.second == b.second) return false;
+		if(lessThan.count(ii(a.second, b.second))) return true;
+		if(lessThan.count(ii(b.second, a.second))) return false;
 		double dx = a.first(*x) - b.first(*x);
-		return dx < 0 || (fabs(dx) < EPS && a.second < b.second);
+		return dx < -EPS || (fabs(dx) < EPS && a.second < b.second);
 	}
 };
 double *sweepLineCmp::x = nullptr;
 int sweepLineCmp::A = -1;
 int sweepLineCmp::B = -1;
+set<ii> sweepLineCmp::lessThan = set<ii>();
 vector<event> intersections(vector<line> &segs) {
 	vector<event> res; priority_queue<event> Q;
 	for(int i = 0; i < segs.size(); i++)
 		Q.push(event(segs[i].p, START, i)),
 		Q.push(event(segs[i].q, END, i));
-	double X = 0; sweepLineCmp::x = &X;
-	set<idSeg, sweepLineCmp> sweep;
+	double X = Q.top().p.real(); sweepLineCmp::x = &X;
+	set<idSeg, sweepLineCmp> sweep; set<point, cmplxCmpG> seen;
 	while(!Q.empty()) {
 		event ev = Q.top(), neu; Q.pop();
+		if(seen.count(ev.p)) continue;
+		seen.insert(ev.p);
 		line tmpL = segs[0]; point tmpP;
 		if(ev.t == START) {
 			auto insertion = sweep.insert(idSeg(segs[ev.ref], ev.ref));
@@ -58,30 +61,35 @@ vector<event> intersections(vector<line> &segs) {
 				neu.ref = r->second, neu.sref = ev.ref,
 				Q.push(neu), res.push_back(neu);
 		} else if(ev.t == END) {
+			if(fabs(X-ev.p.real()) > EPS)
+				sweepLineCmp::lessThan.clear();
 			sweep.erase(idSeg(segs[ev.ref], ev.ref));
 		} else {
 			auto s = sweep.find(idSeg(segs[ev.ref], ev.ref)), t = s;
+			assert(s != sweep.end());
 			t++;
+			assert(t != sweep.end());
 			auto r = s, u = t;
 			if(s != sweep.begin() && intersectSS(segs[t->second], segs[(--r)->second], tmpP))
-				neu = event(tmpP, INTERS, res.size()),
-				neu.ref = r->second, neu.sref = t->second,
-				Q.push(neu), res.push_back(neu);
+				if(tmpP.real() > X)
+					neu = event(tmpP, INTERS, res.size()),
+					neu.ref = r->second, neu.sref = t->second,
+					Q.push(neu), res.push_back(neu);
 			if(++u != sweep.end() && intersectSS(segs[s->second], segs[u->second], tmpP))
-				neu = event(tmpP, INTERS, res.size()),
-				neu.ref = s->second, neu.sref = u->second,
-				Q.push(neu), res.push_back(neu);
+				if(tmpP.real() > X)
+					neu = event(tmpP, INTERS, res.size()),
+					neu.ref = s->second, neu.sref = u->second,
+					Q.push(neu), res.push_back(neu);
 			// swap s & t
 			int tid = t->second, sid = s->second;
 			sweep.erase(idSeg(segs[tid], tid));
 			sweep.erase(idSeg(segs[sid], sid));
-			sweepLineCmp::A = tid;
-			sweepLineCmp::B = sid;
+			sweepLineCmp::lessThan.insert(ii(tid, sid));
 			sweep.insert(idSeg(segs[tid], tid));
 			sweep.insert(idSeg(segs[sid], sid));
-			sweepLineCmp::A = sweepLineCmp::B = -1;
 		}
-		X = ev.p.real();
+		if(Q.empty() || Q.top().p.real() > ev.p.real())
+			X = ev.p.real();
 	}
 	return res;
 }
