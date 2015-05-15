15307303b506e57e2803b754e6647e61ea1d9ec7
MohamedBassem <medoox240@gmail.com>
2015-05-15 12:42:40 +0200

    Adding BS

--- cpp/binary_search.cpp
+++ cpp/binary_search.cpp
@@ -1,15 +1,8 @@
-// Algorithm
-// Algorithm
-// Algorithm
-// Algorithm
-// Algorithm
-// Algorithm
-// Algorithm
-// Algorithm
-// Algorithm
-// Algorithm
-// Algorithm
-// Algorithm
-// Algorithm
-// Algorithm
-// Algorithm
+int lo=0, hi= MAX_VAL+1;
+while(lo<hi){
+  int mid = lo + (hi-lo)/2;
+  if(f(mid))
+    hi = mid
+  else
+    lo = mid+1;
+}
