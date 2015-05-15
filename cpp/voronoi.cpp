41e6a06e867dc029b5c1f27e7543314254406039
Rami Khalil <thesavagekoder@gmail.com>
2015-05-15 12:31:47 +0200

    Voronoi

--- cpp/voronoi.cpp
+++ cpp/voronoi.cpp
@@ -1,20 +1,15 @@
-// Algorithm
-// Algorithm
-// Algorithm
-// Algorithm
-// Algorithm
-// Algorithm
-// Algorithm
-// Algorithm
-// Algorithm
-// Algorithm
-// Algorithm
-// Algorithm
-// Algorithm
-// Algorithm
-// Algorithm
-// Algorithm
-// Algorithm
-// Algorithm
-// Algorithm
-// Algorithm
+void voronoi(poly &pnts, poly& rect, vector<poly> &res){
+	res.clear();
+	for (int i = 0; i < pnts.size(); i++) {
+		res.push_back(rect);
+		for (int j = 0; j < pnts.size(); j++) {
+			if (j == i)
+				continue;
+			point 	p = perp(vec(pnts[i], pnts[j])),
+					m = mid(pnts[i], pnts[j]), mp = m+p;
+			vector<point> temp;
+			polygonCut(res.back(), m, mp, temp);
+			res.back() = temp;
+		}
+	}
+}
