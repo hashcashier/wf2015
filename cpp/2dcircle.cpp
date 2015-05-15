0e037ce10b443eb61c3255129dfe9c2ff66aa84f
Rami Khalil <thesavagekoder@gmail.com>
2015-05-15 11:39:06 +0200

    latex bugs

--- cpp/2dcircle.cpp
+++ cpp/2dcircle.cpp
@@ -4,21 +4,3 @@
 // Algorithm
 // Algorithm
 // Algorithm
-// From 1 Line 2 Points
-// Algorithm
-// Algorithm
-// Algorithm
-// Algorithm
-// Algorithm
-// From 2 Lines 1 Point
-// Algorithm
-// Algorithm
-// Algorithm
-// Algorithm
-// Algorithm
-// From 3 Lines
-// Algorithm
-// Algorithm
-// Algorithm
-// Algorithm
-// Algorithm
